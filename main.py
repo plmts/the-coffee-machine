275d434a4293e09f41e62dbcdb495362e6812014
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-05-25 15:35:10 -0300

    just adding new features. Calling funcctioons to take money and processes with the drink cost, and them return de change

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ MENU = {
         "cost": 3.0,
     }
 }
+profit = 0
 
 resources = {
     "water": 300,
@@ -31,9 +32,8 @@ resources = {
 }
 
 
-
 def is_resources_sufficient(order_ingredients):
-    """Return true when the order can be made and false if ingredients ar insufficient"""
+    """retorna verdadeiro quando o pedido pode ser feito e falso quando não tiver ingredientes suficientes."""
     for item in order_ingredients:
         if order_ingredients[item] >= resources[item]:
             print(f"Sorry there is not enough {item}.")
@@ -41,6 +41,7 @@ def is_resources_sufficient(order_ingredients):
     return True
 
 def process_coins():
+    """Faz o processamentos das moedas inseridas, soma o total e retorna esse valor"""
     total = 0
     print("Please, insert coins.")
     total += int(input("How manny quarters: ")) * 0.25
@@ -49,7 +50,19 @@ def process_coins():
     total += int(input("How manny pennies: ")) * 0.01
     return total
 
-balance = 0
+def transaction_is_successful(money_received, drink_cost):
+    """Retorna verdadeiro quando o valor na máquina cobrir o valor da bebida. 
+    Retorna falso quando não há dinheiro suficiente"""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} as change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry, there is not enouth money! Money refunded.")
+        return False
+
 game_on = True
 
 while game_on:
@@ -60,9 +73,9 @@ while game_on:
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: ${balance}")
+        print(f"Money: ${profit}")
     else:
         drink = MENU[choice]
         if is_resources_sufficient(drink["ingredients"]):
-            process_coins()
-            
+            payment = process_coins()
+            transaction_is_successful(payment, drink["cost"])
